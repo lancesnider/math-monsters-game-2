e0f37cc5c047222d2d160db1099ff498fa0498ac
Lance <lance@lancesnider.com>
2017-10-13 14:10:05 -0700

    Characters and event system

---
+++ Assets/game manager/EventManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+  private GameObject currentObject = null;
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+      currentObject = CheckHit();
+      if(currentObject != null) {
+        currentObject.SendMessage("HandleMouseDown");
+      }
+    } else if (Input.GetMouseButtonUp(0)) {
+      GameObject checkHit = CheckHit();
+
+      if (currentObject!= null & checkHit == currentObject) {
+        currentObject.SendMessage("HandleClick");
+      }else if (currentObject!= null) {
+        currentObject.SendMessage("HandleReleaseOutside");
+      }
+      currentObject = null;
+    }
+	}
+
+
+  GameObject CheckHit () {
+    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+    if (hit.collider != null) {
+      return hit.collider.gameObject;
+    }
+    return null;
+  }
+}
